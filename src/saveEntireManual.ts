aa83d90b12e6cf8a02a27b870eb2a7179ab68526
Sam <iamtheyammer@gmail.com>
2024-02-26 16:13:36 +0000

    Remove illegal characters from filenames, fixes #9

--- src/saveEntireManual.ts
+++ src/saveEntireManual.ts
@@ -5,6 +5,21 @@ import client from "./client";
 import { createWriteStream } from "fs";
 import { Page } from "playwright";
 import saveStream from "./saveStream";
+import { type as osType } from "os";
+
+// NTFS and FAT have many restricted characters in filenames.
+// We need to remove these here because keys in this
+// tree will be used to create file and folder names.
+// https://en.wikipedia.org/wiki/Filename#Comparison_of_filename_limitations
+
+// Slashes are not included here so that the names in cover.html match the ones here.
+// They are removed in saveEntireManual as files are written to disk.
+
+// Emdashes (\u2013) are included as they are multi-byte and throw off length
+// calculations where 1 character is expected to be 1 byte.
+const nameRegex =
+  osType() === "Windows_NT" ? /[<>:"\\/|?*\0\u2013]/g : /[\\/\0\u2013]/g;
+const sanitizeName = (name: string): string => name.replace(nameRegex, "-");
 
 export default async function saveEntireManual(
   path: string,
@@ -44,20 +59,17 @@ export default async function saveEntireManual(
       }
 
       console.log(`Downloading manual page ${name}.html (docID: ${docID})`);
-      // remove slashes, emdashes (because they are multi-byte), and colons
-      const sanitizedName = name.replace(/[\/\u2013:]/g, "-");
-      let filename = sanitizedName;
+      let filename = sanitizeName(name);
 
       // 255 is the max filename length on most filesystems, but 200 should be enough regardless
-      if (sanitizedName.length > 200) {
+      if (filename.length > 200) {
         filename =
           // 255 = max filename length, 18 = length of " ( truncated).html",
           // docID.length = length of docID
 
           // including the docID in the filename to prevent collisions as names may differ
           // at the end rather than in the first ~255 characters
-          sanitizedName.slice(0, 254 - 19 - docID.length) +
-          ` (${docID} truncated)`;
+          filename.slice(0, 254 - 19 - docID.length) + ` (${docID} truncated)`;
 
         console.log(`-> Truncating filename, learn more in the README`);
       }
@@ -78,7 +90,7 @@ export default async function saveEntireManual(
       );
     } else {
       // create folder and traverse
-      const newPath = join(path, name);
+      const newPath = join(path, sanitizeName(name));
 
       try {
         await mkdir(newPath, { recursive: true });
